2d97297077a41cd35d0a3df19a87c5ddc8d7c900
Ghanshyam Fulumbrikar <ghanshyamfulumbrikar121@gmail.com>
2026-01-26 17:34:42 +0530

    Added Transaction and OTP service

---
+++ OPTService/src/main/java/com/banking_system/OPTService/OptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking_system.OPTService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptServiceApplication.class, args);
+	}
+
+}
---
+++ OPTService/src/test/java/com/banking_system/OPTService/OptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking_system.OPTService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
