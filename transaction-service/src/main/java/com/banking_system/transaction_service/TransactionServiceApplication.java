52bc629687d5fadda01b4becaaf549c02123efe7
Ghanshyam Fulumbrikar <ghanshyamfulumbrikar121@gmail.com>
2026-01-26 19:01:40 +0530

    Configure Swagger API and API routes in Api Gateway Service

--- transaction-service/src/main/java/com/banking_system/transaction_service/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/banking_system/transaction_service/TransactionServiceApplication.java
@@ -2,11 +2,13 @@ package com.banking_system.transaction_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
